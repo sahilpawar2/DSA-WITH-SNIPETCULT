8b890262c5272a643c8fe35643fa482cf458d899
@snipetcult <pawarsahil1306@gmail.com>
2024-11-16 02:38:54 -0800

    new changes

--- 01_array_datastructure/04_ARRAY-QUESTIONS/questoin.js
+++ 01_array_datastructure/04_ARRAY-QUESTIONS/questoin.js
@@ -15,3 +15,30 @@
 
 
 
+// function threeSum(nums) {
+//     let i = 0
+//     let j = i+1
+//     let k = nums.length-1
+//     let result = []
+//     while(i < k){
+//         let sum = 0
+//         sum = nums[i] + nums[j] + nums[k]
+//         if(sum === 0){
+//             let res = [nums[i], nums[j], nums[k]]
+//             result.push(res)
+//             return result;
+//         }else if(sum > 0){
+//             --k
+//         } else if(sum < 0){
+//             ++j
+//         }else{
+//             ++i
+//         }
+//     }
+//     return
+// };
+// console.log(threeSum([-1,0,1,2,-1,-4]))
+
+let sahil = [-1,0,1,2,-1,-4]
+sahil.sort((a, b)=>   a -b)
+console.log(sahil);
